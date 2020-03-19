65924d2ea52c1304c3fad8bcf822807711b7aa7e
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-19 15:02:19 +0200

    4(Generating Dummy Data using Factory عمل سيد لليوزر+الكاتيجوري+البوست)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
+use App\Post;
+use App\Category;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -23,5 +25,29 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+
+    ];
+});
+
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'category_id' => function(){
+            return factory(Category::class)->create()->id;
+        },
+        'user_id' => function(){
+            return factory(User::class)->create()->id;
+        },
+        'title'=>$faker->sentence,
+        'description'=>$faker->paragraph,
+        'photo'=>$faker->imageUrl,
+
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Post;
+use App\Category;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(Post::class,10)->create();
+        factory(Category::class,10)->create();
+        factory(User::class,10)->create();
     }
 }
