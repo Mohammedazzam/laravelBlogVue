f98587daf5dbeca32f76ce1cb4a089f562305c32
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-20 19:58:43 +0200

    5(Creating Controllers for Post and Cateory +image.interventionتحميل مكتبة )

---
+++ app/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+Use App\Category;
+Use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class,'category_id');
+    }
+}
